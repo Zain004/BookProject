96a06c201d4bfdf137f7aca3e147bcb7677e9817
Zain004 <143086801+Zain004@users.noreply.github.com>
2025-03-31 22:22:16 +0200

    Ferdig med indes.html i users klassen

--- Eksamen2024Hoved/versjon2/src/main/java/com/example/versjon2/ExamUserTask/Controller/UsersController.java
+++ Eksamen2024Hoved/versjon2/src/main/java/com/example/versjon2/ExamUserTask/Controller/UsersController.java
@@ -35,7 +35,7 @@ public class UsersController {
      * @param users
      * @return
      */
-    @PostMapping("/saveUser")
+    @PostMapping()
     public ResponseEntity<APIResponse<UsersDTO>> saveUser(@RequestBody @Valid Users users) {
         String requestId = UUID.randomUUID().toString(); // legger til en unik id for sporbarhet
         MDC.put("requestId", requestId);
