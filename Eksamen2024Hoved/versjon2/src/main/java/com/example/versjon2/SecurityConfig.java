b0c6e5d421c5d9b4014c1acc55b9735cbbc51967
Zain004 <143086801+Zain004@users.noreply.github.com>
2025-04-09 12:40:37 +0200

    fikset h2 database

--- Eksamen2024Hoved/versjon2/src/main/java/com/example/versjon2/SecurityConfig.java
+++ Eksamen2024Hoved/versjon2/src/main/java/com/example/versjon2/SecurityConfig.java
@@ -18,7 +18,6 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean(name = "authenticationSecurityFilterChain")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -33,6 +32,30 @@ public class SecurityConfig {
 
         return http.build();
     }
+/*
+    @Bean(name = "authenticationSecurityFilterChain")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF (ONLY for local testing)
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll() // Tillat H2-konsollen (KUN i dev!)
+                        .anyRequest().permitAll() //La til en kommentar
+                )
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.DENY))
+                        .addHeaderWriter((request, response) -> {
+                            response.setHeader("Report-To", "{ \"group\": \"csp-endpoint\", \"max_age\": 10886400, \"endpoints\": [ { \"url\": \"/csp-report-endpoint\" } ] }");
+                            response.setHeader("Strict-Transport-Security", "max-age=31536000 ; includeSubDomains; preload");
+                            response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+                            response.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+                            response.setHeader("Permissions-Policy", "geolocation=(), microphone=(), camera=(), payment=(), usb=(), gyroscope=(), magnetometer=(), picture-in-picture=(), display-capture=(), fullscreen=(self)");
+                        })
+                );
+
+        return http.build();
+    }
+
+ */
 
     @Bean // Bean er oppskrifter på hvordan lage ting, når den er laget brukes den til andre ting
     public PasswordEncoder passwordEncoder() {
